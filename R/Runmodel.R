78969c9024a9fe0259ef1f9fb70af54d5ae33dc6
KPidd <120022402+KPidd@users.noreply.github.com>
2023-06-29 12:11:27 +0100

    Replace old .r with Runmodel.r

--- R/Runmodel.R
+++ R/Runmodel.R
@@ -1,24 +1,3 @@
-#    Embedding RCT Health Economic Analysis using the Sheffield Type 2 Diabetes Treatment Model - version 3
-#    Copyright (C) 2023   Pollard, Pidd, Breeze, Brennan, Thomas
-
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
-
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-#    Contact person: Dan Pollard, Email: d.j.pollard@sheffield.ac.uk, 
-#    Address: Regent Court, 30 Regent Court, Sheffield, United Kingdom, S1 4DA
-
-
 ####'This function runs the model for a given set of patients and parameters
 ####'@param population_, is the population matrix
 ####'@param parameters_, is one row of the parameters matrix
@@ -52,6 +31,7 @@ run_model <- function(population_,
                                      1)
   }else{#otherwise the model is PSA
     PSAiterations <- 2:(as.numeric(GlobalVars_["psa_count","Value"])+1)
+    
     
     #set up parrallel processing
     #these are the additional global variables that need to be set for parrallel
